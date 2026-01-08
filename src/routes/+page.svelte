e1850086e7516e3344e603deeac1de8b98b3a642
John Schoneboom <john.schoneboom@gmail.com>
2026-01-08 11:42:18 +0000

    deleting unused styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,34 +52,23 @@
 </div>
 <style>
     h3 {font-size: 1.5em; margin: 1em;}
-#homecards {width: 95%; margin: auto; margin-top: 20px; margin-bottom: 40px;}
-#homecards:after {content:"";clear:both;}
-.eprase-icons {color:#fff!important;background-color:#7e8cdd!important;}
-.eprase-icons:hover {background-color: #3e53cc!important;}
-.clickable {cursor: pointer;}
-.clickable:hover {box-shadow:0 8px 16px 0 rgba(0,0,0,0.2);}
-.w3-card {border: 1px solid #eee; box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)}
-.row {width: 100%; display: table; text-align: center;}
-.col {display: table-cell;}
-.col-1 {width: 8.33%;}
-.col-2 {width: 16.66%;}
-.col-3 {width: 25%;}
-.col-4 {width: 33.33%;}
-.col-5 {width: 41.66%}
-.col-6 {width: 50%;}
-.col-7 {width: 58.33%;}
-.col-8 {width: 66.66%;}
-.col-9 {width: 75%;}
-.col-10 {width: 83.33%;}
-.col-11 {width: 91.66%;}
-.col-12 {width: 100%;}
+    #homecards {width: 95%; margin: auto; margin-top: 20px; margin-bottom: 40px;}
+    #homecards:after {content:"";clear:both;}
+    .eprase-icons {color:#fff!important;background-color:#7e8cdd!important;}
+    .eprase-icons:hover {background-color: #3e53cc!important;}
+    .clickable {cursor: pointer;}
+    .clickable:hover {box-shadow:0 8px 16px 0 rgba(0,0,0,0.2);}
+    .w3-card {border: 1px solid #eee; box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)}
+    .row {width: 100%; display: table; text-align: center;}
+    .col {display: table-cell;}
+    .col-4 {width: 33.33%;}
 
-[class*="col-"] {
-    float: left;
-}
-@media screen and (max-width: 768px) {
-  .col {
-    width: 100%;
-  }
-}
+    [class*="col-"] {
+        float: left;
+    }
+    @media screen and (max-width: 768px) {
+    .col {
+        width: 100%;
+    }
+    }
 </style>
