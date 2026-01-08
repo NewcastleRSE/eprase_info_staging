df393519715873a90c54edc86b7c8fed6f7bfdcd
John Schoneboom <john.schoneboom@gmail.com>
2026-01-08 11:09:31 +0000

    more build fixes

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import strict from 'node:assert/strict';
 
-/** @type {import('@sveltejs/kit').Config} */
+@type {import('@sveltejs/kit').Config}
 
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
