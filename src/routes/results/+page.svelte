66162f8e3a88deba3e1c94560f4151d36acb2967
John Schoneboom <john.schoneboom@gmail.com>
2026-04-28 12:02:52 +0100

    adding new results tabs

---
+++ src/routes/results/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // Sends them to 2025 immediately
+        goto(`/results/2025`, { replaceState: true });
+    });
+
+</script>
+
