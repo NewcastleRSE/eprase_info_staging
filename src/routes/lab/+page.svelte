7212f02e0df84a3ab6913deacde363218e6e4298
John Schoneboom <john.schoneboom@gmail.com>
2026-06-24 09:43:42 +0100

    fix broken link

--- src/routes/lab/+page.svelte
+++ src/routes/lab/+page.svelte
@@ -4,5 +4,5 @@
 </script>
 <div class="subpage">
     <p>The Electronic Prescribing Learning Lab is an interactive learning resource developed to support digital and pharmacy teams to optimise their electronic prescribing systems. View below or <a href={resolve("/pdf/learning_lab.pdf")} target="_blank">download the PDF</a>.</p>
-    <PdfViewer src={resolve("/pdf/learning_lab.pdf")} title="Learning Lab" />
+    <PdfViewer src="/pdf/learning_lab.pdf" title="Learning Lab" />
 </div>
